ca111b72bb0408a2ddf2dbdbcdd7360b8122e49d
Harun Gachanja <arrotechdesign@gmail.com>
2021-03-22 23:56:07 +0300

    redirect to home page

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 import os
+from flask import redirect, url_for
 from app import create_app
 
 socketio, app = create_app(os.environ.get("FLASK_ENV"))
+
+
+@app.route("/")
+def index():
+    return redirect(url_for('chat_v1.home'))
+
 
 if __name__ == "__main__":
     socketio.run(app)
