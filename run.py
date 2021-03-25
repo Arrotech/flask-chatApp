579b3d753a7c4b734c2bb2f0c618d91eb84ad73f
Harun Gachanja <arrotechdesign@gmail.com>
2021-03-25 04:29:51 +0300

    Create Tables

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from flask import redirect, url_for
 from app import create_app
+from app.extensions import db
 
 socketio, app = create_app(os.environ.get("FLASK_ENV"))
 
@@ -8,6 +9,14 @@ socketio, app = create_app(os.environ.get("FLASK_ENV"))
 @app.route("/")
 def index():
     return redirect(url_for('chat_v1.home'))
+
+
+@app.cli.command()
+def create_tables_v1():
+    """Version 1: Create tables if they do not exists."""
+    print("Creating tables...")
+    db.create_all()
+    print("Version 1 new tables created... OK")
 
 
 if __name__ == "__main__":
